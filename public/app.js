6f6eee88f3f651720ef821b77e62e179a0071d63
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-10-03 00:23:51 +0100

    now can search for playlists based on genres

--- public/app.js
+++ public/app.js
@@ -2,23 +2,29 @@ var app = function(){
   var button = document.getElementById("submit");
   button.onclick = function() {
     var genre = document.getElementById("input").value;
-    var url = 'https://api.spotify.com/v1/search?q=' + genre + '&type=playlist';
+    var url = 'https://api.spotify.com/v1/search?q='+ genre +'&type=playlist';
     makeRequest(url, requestComplete);
   }.bind(this);
 }
 
-var makeRequest = function(url, callback){
+var makeRequest = function(url, callback) {
   var request = new XMLHttpRequest();
   request.open("GET", url);
   request.onload = callback;
   request.send();
 }
 
-var requestComplete = function(){
+var requestComplete = function() {
   if(this.status !== 200) return;
   var inputValue = this.responseText;
   var search = JSON.parse(inputValue);
-  console.log(inputValue);
+  var genre = document.getElementById('input').value;
+  console.log(search);
+  // var playlists;
+  // for(playlist of search) {
+  //   var displayResults = document.getElementById('results');
+  //   displayResults.innerText = playlist;
+  // }
 }
 
 window.onload = app;
